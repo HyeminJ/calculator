fe3fc8e7bc744d5ec29ded211fe9a63842554976
HyeminJ <gpals5643@gmail.com>
2019-08-19 09:52:59 +0900

    곱하기 메소드 추가

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -14,4 +14,10 @@ public class Calculator {
     public double divide(double a, double b) {
         return a / b;
     }
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+    public int multiply(int a, int b) {
+        return a*b;
+    }
 }
