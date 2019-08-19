94bc6fd21691056f2450c4b5e231bf52000ba4e1
HyeminJ <gpals5643@gmail.com>
2019-08-19 10:43:41 +0900

    곱셈 테스트케이스 추가

--- src/main/java/com/example/calculator/CalculatorTest.java
+++ src/main/java/com/example/calculator/CalculatorTest.java
@@ -56,8 +56,8 @@ public class CalculatorTest {
     @Test
     public void test_곱하기() {
         Calculator calc = new Calculator();
-        double result = calc.multiply(10,3);
-        assertEquals("두 곱셈의 값은 30이어야 합니다.", 30, result);
+        double result = calc.multiply(10,4);
+        assertEquals("두 곱셈의 값은 40이어야 합니다.", 40, result);
     }
 }
 
